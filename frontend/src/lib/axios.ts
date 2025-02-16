d9acad138dddb9531b5f9d2f1e7f841662944020
Ravi Karmakar <ravikarmkar94475@gmail.com>
2025-02-17 02:23:51 +0530

    fixed a backend endpoint

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:9001/api",
-  // baseURL: "https://raviportfolio.krmsolutions.in/api",
+  // baseURL: "http://localhost:9001/api",
+  baseURL: "https://raviportfolio.krmsolutions.in/api",
 });
